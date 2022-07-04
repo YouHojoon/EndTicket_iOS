f22ac83bc3ef0e0ebfb0cff9122758543b14856d
YouHojoon <dbghwns66@daum.net>
2022-07-04 15:46:16 +0900

    fix:HomeView - 닉네임 상단에 나오도록 변경

--- EndTicket/Tab/Home/HomeView.swift
+++ EndTicket/Tab/Home/HomeView.swift
@@ -34,7 +34,7 @@ struct HomeView: View {
                 }
                 .padding(.bottom,23)
                
-                Text("드리밍님\n오늘도 같이 도전해볼까요?")
+                Text("\(UserDefaults.standard.string(forKey:"nickname")!)님\n오늘도 같이 도전해볼까요?")
                     .font(.interBold(size: 22))
                 HStack(spacing:0){
                     Image(systemName: "clock")
