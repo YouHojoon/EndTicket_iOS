42a9564af7b09d5926293114cacf26a5ee92445f
YouHojoon <dbghwns66@daum.net>
2022-07-07 20:02:20 +0900

    fix:OnBoardingView, OnBoarding - 디자인 변경으로 인한 수정

--- EndTicket/OnBoarding/Enum/OnBoarding.swift
+++ EndTicket/OnBoarding/Enum/OnBoarding.swift
@@ -10,29 +10,40 @@ import SwiftUI
 
 //MARK: - OnBoardingView 관련 정리
 @frozen enum OnBoarding: CaseIterable{
-    case home
-    case futureOfMe
-    case recommendationGoal
+    case first
+    case second
+    case third
     
     var title: String{
         switch self {
-        case .home:
-            return "티켓을 터치할 때마다 목적지에\n가까워질 수 있어요!\n티켓을 만들어보고 목표를 달성해보세요."
-        case .futureOfMe:
-            return "미래의 나는 어떨지 상상해보고\n목표를 미리 설정해봐요!\n그리고 캐릭터와 타이틀을 통해\n변화를 느껴보세요 :)"
-        case .recommendationGoal:
-            return "어떤걸 해야할 지 생각이 안난다면\n추천목표를 통해 다른 사람들의 목표를 참고해봐요!"
+        case .first:
+            return "목표에 다가가기"
+        case .second:
+            return "미래의 나는 어떨까?"
+        case .third:
+            return "다른 사람과 공유해요!"
+        }
+    }
+    
+    var content: String{
+        switch self {
+        case .first:
+            return "티켓을 오른쪽으로 스와이프하면 달성\n왼쪽으로 스와이프하면 취소를 할 수 있어요!"
+        case .second:
+            return "나를 상상해보고 목표를 미리 설정해봐요!\n그리고 경험치를 얻어 변화를 느껴보세요:)"
+        case .third:
+            return "내가 달성한 목표를 공유하고\n다른 사람들의 티켓을 추가해봐요."
         }
     }
     
     var image: Image{
         switch self{
-        case .home:
-            return Image("on_boarding_1")
-        case .futureOfMe:
-            return  Image("on_boarding_2")
-        case .recommendationGoal:
-            return  Image("on_boarding_3")
+        case .first:
+            return Image("onBoarding_1")
+        case .second:
+            return  Image("onBoarding_2")
+        case .third:
+            return  Image("onBoarding_3")
         }
     }
     
