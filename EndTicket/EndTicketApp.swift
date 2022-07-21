548759265dc4da0e1a6d1275af8cf6abfafcae9b
YouHojoon <dbghwns66@daum.net>
2022-07-21 21:55:28 +0900

    add:푸시 알림 기능 추가

--- EndTicket/EndTicketApp.swift
+++ EndTicket/EndTicketApp.swift
@@ -4,7 +4,8 @@
 //
 //  Created by 유호준 on 2022/05/27.
 //
-
+import FirebaseCore
+import FirebaseMessaging
 import SwiftUI
 import GoogleSignIn
 import KakaoSDKCommon
@@ -15,7 +16,8 @@ import KakaoSDKUser
 struct EndTicketApp: App {
     private let googleClientId: String
     static let baseUrl = "https://dev.endticket.shop/"
-
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     init(){
         guard let cetentialListFile = Bundle.main.url(forResource: "Credential", withExtension: "plist"), let credentialList = NSDictionary(contentsOf: cetentialListFile) else{
             fatalError("SNS Login을 위한 Credential.plist가 존재하지 않습니다.")
@@ -30,15 +32,74 @@ struct EndTicketApp: App {
     var body: some Scene {
         WindowGroup {
             SignInView()
-            .onOpenURL{
-                if AuthApi.isKakaoTalkLoginUrl($0){
-                    AuthController.handleOpenUrl(url: $0)
+                .onOpenURL{
+                    if AuthApi.isKakaoTalkLoginUrl($0){
+                        AuthController.handleOpenUrl(url: $0)
+                    }
+                    else{
+                        GIDSignIn.sharedInstance.handle($0)
+                    }
                 }
-                else{
-                    GIDSignIn.sharedInstance.handle($0)
-                }
-            }
-            .environmentObject(SignInViewModel(googleClientId: googleClientId))
+                .environmentObject(SignInViewModel(googleClientId: googleClientId))
         }
     }
 }
+
+
+
+
+class AppDelegate: NSObject,UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        if #available(iOS 10.0, *){
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions){_, _ in
+            }
+        }
+        else{
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert,.badge,.sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+      
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("device token \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+}
+
+
+
+extension AppDelegate: MessagingDelegate{
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("AppDelgate - 파이어베이스 토큰 수신")
+        print("AppDelgate - Firebase registration token: \(String(describing: fcmToken))")
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        print("will present: userInfo: ", userInfo)
+        let categori = notification.request.content.categoryIdentifier
+        print("categori : \(categori)")
+        completionHandler([.badge, .banner, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        print("didReceive : userInfo: ", userInfo)
+        print("pressed \(response.actionIdentifier)")
+        completionHandler()
+    }
+}
+
+
+
+
